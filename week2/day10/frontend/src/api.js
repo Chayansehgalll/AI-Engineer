39c35ea7a45a6520924bf524f2f758d75e40df9c
ChayanSehgalll <chayansehgal2971@gmail.com>
2026-08-02 11:30:08 +0530

    FE-update

--- week2/day10/frontend/src/api.js
+++ week2/day10/frontend/src/api.js
@@ -1,9 +1,11 @@
-import axios from "axios";
-
+const API_URL = "https://ai-engineer-f0ri.onrender.com";
 export async function sendMessage(message) {
-  const response = await axios.post("http://localhost:8000/chat", {
-    message,
+  const res = await fetch(`${API_URL}/chat`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message }),
   });
 
-  return response.data.answer;
+  const data = await res.json();
+  return data.answer;
 }
